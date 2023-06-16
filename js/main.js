6ad83abe227d1667c94e793a608e89f1463ea53a
colas-sebastien <colas.sebastien@free.fr>
2023-06-16 08:50:15 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+function display(map)
+{
+    html="<table>";
+    for (y=0;y<map.height;y++)
+    {
+        html+="<tr>";
+        for (x=0;x<map.width;x++)
+        {
+            html+="<td  class='";
+            switch (map.data[y][x])
+            {
+                case "C":
+                    html+="church";
+                    break;                
+                case "F":
+                    html+="forest";
+                    break;
+                case "M":
+                    html+="montain";
+                    break;                    
+                case "W":
+                    html+="well";
+                    break;                    
+                case "R":
+                    html+="rocks";
+                    break;                
+                case "T":
+                    html+="tree";
+                    break;
+                case "U":
+                    html+="trees";
+                    break;
+                case "X":
+                    html+="water";
+                    break;                                   
+                default:                    
+                    break;
+            }
+            switch (map.river[y][x])
+            {                
+                case "B":
+                    html+="'><img src='../img/river_b.png";
+                    break;
+                case "BR":
+                    html+="'><img src='../img/river_br.png";
+                    break;
+                case "BT":
+                    html+="'><img src='../img/river_bt.png";
+                    break;
+                case "BTR":
+                    html+="'><img src='../img/river_btr.png";
+                    break;
+                case "BTL":
+                    html+="'><img src='../img/river_btl.png";
+                    break;                                                                
+                case "L":
+                    html+="'><img src='../img/river_l.png";
+                    break;                
+                case "R":
+                    html+="'><img src='../img/river_r.png";
+                    break;
+                case "T":
+                    html+="'><img src='../img/river_t.png";
+                    break;                       
+                case "TL":
+                    html+="'><img src='../img/river_tl.png";
+                    break;    
+                case "TR":
+                    html+="'><img src='../img/river_tr.png";
+                    break;    
+                case "E":                    
+                    break;                                                                
+                default:                    
+                    html+="'><img src='../img/box.png";
+                    break;
+            }                 
+            html+="'></td>";
+        }
+        html+="</tr>";
+    }
+    html+="</table>";
+    
+    $('#map').html(html);
+    
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+# My City Roll & Write: Maps
+
+## Resources
+
+- https://www.svgrepo.com/svg/398225/rock
+- https://www.svgrepo.com/svg/475460/tree
+- https://www.svgrepo.com/svg/228573/well
+- https://www.svgrepo.com/svg/404964/church
+
+## Chapter 1 - Episode 1
+![map1](img/map_1.png)
+
+## Chapter 1 - Episode 2
+![map1](img/map_2.png)
+
+## Chapter 1 - Episode 3
+![map1](img/map_3.png)
+
+## Chapter 2 - Episode 4
+![map1](img/map_4.png)
+
+## Chapter 2 - Episode 5
+![map1](img/map_5.png)
+
+## Chapter 2 - Episode 6
+![map1](img/map_6.png)
